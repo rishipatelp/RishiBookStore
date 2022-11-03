4ad247cdea149cac0bc68b8f85f26dc2e49983a2
rishipatelp <W0793722@myscc.ca>
2022-11-03 10:34:11 -0400

    Created 3 new projects

---
+++ RishiBooks.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RishiBooks.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ RishiBooks.Utility/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RishiBooks.Utility
+{
+    public class Class1
+    {
+    }
+}
